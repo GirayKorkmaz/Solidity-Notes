9004d4c01fbccb1bd14bf09be2313a546ba6ddcc
Leo <123651761+GirayKorkmaz@users.noreply.github.com>
2024-05-13 21:58:18 +0300

    Add files via upload

---
+++ WhileLoop.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.0;
+
+contract Loops {
+    uint256[15] public numbers0;
+    uint256[15] public numbers1;
+     function listByFor() public {
+        uint256[15] memory nums=numbers0;
+       
+
+        for (uint256 i=0; i<nums.length ; i++) {
+            if (i==9) break ; 
+            if (i==9) continue ; 
+            nums[i]=i;
+
+        }
+         numbers0=nums;
+       
+     }
+        function getArr0() public view returns (uint256[15] memory) {
+    return numbers0;
+    }
+     }
+    
